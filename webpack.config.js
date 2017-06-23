554a66bed72549562dc69b88148f17fdb1be89f1
Jaime Molina <js.molina13@uniandes.edu.co>
2017-06-23 17:07:19 -0400

    add new configuration

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path')
+const webpack = require('webpack')
+
+const ROOT_DIR = path.resolve('./')
+const JS_SRC_DIR = `${ROOT_DIR}/src`
+const devBuild = process.env.NODE_ENV !== 'production'
+
+if (devBuild) {
+  console.log('Webpack dev build') // eslint-disable-line no-console
+  module.exports.devtool = 'eval-source-map'
+} else {
+  console.log('Webpack production build') // eslint-disable-line no-console
+}
+
+module.exports = {
+  devtool: 'cheap-module-source-map',
+  entry: {
+    app: [
+      'babel-polyfill',
+      `${JS_SRC_DIR}/main.js`
+    ]
+  },
+
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        include: path.resolve(JS_SRC_DIR),
+        exclude: /(node_modules|bower_components)/,
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  },
+
+  plugins: [
+    new webpack.EnvironmentPlugin({ NODE_ENV: 'development' }),
+    new webpack.NamedModulesPlugin()
+  ],
+  output: {
+    path: ROOT_DIR,
+    filename: 'app.js'
+  }
+}
