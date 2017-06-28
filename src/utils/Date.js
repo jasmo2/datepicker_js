694dc05f10c9e5bed3b6a245a438b04cba0bddc4
Jaime Molina <js.molina13@uniandes.edu.co>
2017-06-28 16:26:36 -0400

    add monthName

--- src/utils/Date.js
+++ src/utils/Date.js
@@ -1,6 +1,11 @@
+const monthSNames = ['January', 'February', 'March', 'April', 'May', 'June',
+  'July', 'August', 'September', 'October', 'November', 'December'
+]
+
 class MyDate {
   constructor (props) {
     this.date = props
+
     this.daysInMonth = this.daysInMonth.bind(this)
     this.daysPrevInMonth = this.daysPrevInMonth.bind(this)
   }
@@ -12,6 +17,9 @@ class MyDate {
   daysPrevInMonth () {
     const { date } = this
     return new Date(date.getFullYear(), date.getMonth(), 0).getDate()
+  }
+  monthName () {
+    return monthSNames[this.date.getMonth()]
   }
 }
 
