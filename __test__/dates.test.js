a1ed0d8b4d9145abc5873499fed472183a17cde1
Jaime Molina <js.molina13@uniandes.edu.co>
2017-06-29 14:34:31 -0400

    test CalendarDates

--- __test__/dates.test.js
+++ __test__/dates.test.js
@@ -1,17 +1,24 @@
 import CalendarDates from '../src/js/dates'
 
 const properties = {
-  initialDate: new Date(),
+  initialDate: new Date(2017,1,1),
   start: (new Date()).getFullYear(),
   end: (new Date()).getFullYear()
 }
 
 describe('CalendarDates Class', () => {
-  describe('the constructor', () => {
-    it ('get rows String', () => {
+  describe('"rows" function', () => {
+    it ('count rows', () => {
       const calendar = new CalendarDates(properties)
       const { rowsHtml } = calendar.rows({ initialDate: properties.initialDate })
       expect((rowsHtml.match(/calendar__row/g) || []).length).toEqual(6)
+    })
+    it ('get 31 days for Jan', () => {
+      const calendar = new CalendarDates(properties)
+      const { rowsHtml } = calendar.rows({ initialDate: properties.initialDate })
+      const janDay = Array.from({length: 31}, (v, k) => `${k + 1}`)
+
+      expect(rowsHtml.match(/\d+/g)).toEqual(expect.arrayContaining(janDay))
     })
   })
 })
